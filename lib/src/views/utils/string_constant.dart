162038e875bce6bca7e5e3c0b5acf912b5f4ecaf
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-04-25 18:03:05 +0700

    chore: Update constant values

---
+++ lib/src/views/utils/string_constant.dart
@@ -0,0 +1,11 @@
+class StringConstant {
+  static const String mainFontName = 'SF Pro';
+
+  static const String NEXT_LABEL = 'Next';
+
+  static const String GET_STARTED_LABEL = 'Get Started';
+
+  static const String AGREE_TO_OUR_TERM_AND_PRIVACY_POLICY_CTA_LABEL = 'By signing up, you agree to our';
+
+  static const String TERM_AND_PRIVACY_POLICY_LABEL = 'Terms & Privacy Policy.';
+}
