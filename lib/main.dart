874d9dd0f51999f7e797b4683b00d13ea3c3c084
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-04-25 18:03:48 +0700

    feat: Update Theme data and route

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'src/views/ui/screens/onboarding_screen.dart';
 import 'src/views/utils/route_constant.dart';
+import 'src/views/utils/string_constant.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,9 +14,21 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            fontFamily: StringConstant.mainFontName,
+          ),
+          bodyLarge: TextStyle(
+            fontFamily: StringConstant.mainFontName,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
       initialRoute: RouteConstant.ONBOARDING_SCREEN_ROUTE,
       routes: <String, WidgetBuilder>{
-        RouteConstant.ONBOARDING_SCREEN_ROUTE: (BuildContext context) => const OnboardingScreen(),
+        RouteConstant.ONBOARDING_SCREEN_ROUTE: (BuildContext context) =>
+            const OnboardingScreen(),
       },
     );
   }
