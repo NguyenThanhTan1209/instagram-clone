6438fe10ce1e51a8511783d35a14621f6a47217c
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-07-10 15:55:05 +0700

    fix: Change the variable's data type of Post model

--- lib/src/business_logic/models/post.dart
+++ lib/src/business_logic/models/post.dart
@@ -1,13 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'comment.dart';
+
 class Post {
   Post({
     required this.postID,
-    required this.imagesPath,
-    required this.description,
-    required this.location,
+    required this.userID,
+    required this.userName,
+    required this.images,
+    this.likedUsers = const <User>[],
+    this.comments = const <PostComment>[],
+    this.content = '',
+    this.location = '',
   });
 
-  final int postID;
-  final List<String> imagesPath;
-  final String description;
-  final String location;
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'postID': postID,
+        'userID': userID,
+        'userName': userName,
+        'images': images,
+        'likedUsers': likedUsers,
+        'comments': comments,
+        'content': content,
+        'location': location,
+      };
+
+  String postID;
+  String userID;
+  String userName;
+  List<String> images;
+  List<User> likedUsers;
+  List<PostComment> comments;
+  String content;
+  String location;
 }
