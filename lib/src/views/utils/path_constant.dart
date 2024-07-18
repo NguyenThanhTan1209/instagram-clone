7302043cc6281ef5e39b244acd004f1aa0d8ce77
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-07-18 18:23:33 +0700

    chore: Update constant values

--- lib/src/views/utils/path_constant.dart
+++ lib/src/views/utils/path_constant.dart
@@ -1,40 +1,81 @@
 class PathConstant {
   static const String INSTA_LOGO_PATH = 'assets/images/insta.png';
   static const String META_GROUP_PATH = 'assets/images/meta_group.png';
-  static const String INSTA_BLACK_LOGO_PATH = 'assets/images/insta_black_logo.png';
+  static const String INSTA_BLACK_LOGO_PATH =
+      'assets/images/insta_black_logo.png';
   static const String FACEBOOK_ICON_PATH = 'assets/icons/facebook_icon.png';
   static const String GOOGLE_ICON_PATH = 'assets/icons/google_icon.png';
   static const String USER_ICON_PATH = 'assets/icons/user_icon.png';
   static const String GREEN_TICK_ICON_PATH = 'assets/icons/green_tick_icon.png';
-  static const String PASSWORD_LOCK_ICON_PATH = 'assets/icons/password_lock_icon.png';
-  static const String PASSWORD_HIDDEN_EYE_ICON_PATH = 'assets/icons/password_hidden_eye_icon.png';
-  static const String PASSWORD_SHOWN_EYE_ICON_PATH = 'assets/icons/password_shown_eye_icon.png';
-  static const String EXPLORE_ROUNDED_ICON_PATH = 'assets/icons/explore_rounded_icon.png';
-  static const String HOME_FEED_ROUNDED_ICON_PATH = 'assets/icons/home_feed_rounded_icon.png';
-  static const String NOTIFICATION_ROUNDED_ICON_PATH = 'assets/icons/notification_rounded_icon.png';
-  static const String REELS_ROUNED_ICON_PATH = 'assets/icons/reels_rounded_icon.png';
-  static const String USER_PROFILE_ROUNED_ICON_PATH = 'assets/icons/user_profile_rounded_icon.png';
-  static const String ACCOUNT_LIST_DROPDOWN_ICON_PATH = 'assets/icons/account_list_dropdown_icon.png';
-  static const String ADD_MEDIUM_BUTTON_ICON_PATH = 'assets/icons/add_medium_button_icon.png';
-  static const String INSTAGRAM_ROUNDED_ICON_PATH = 'assets/icons/instagram_rounded_icon.png';
-  static const String PRIVATE_ACCOUNT_ICON_PATH = 'assets/icons/private_account_icon.png';
+  static const String PASSWORD_LOCK_ICON_PATH =
+      'assets/icons/password_lock_icon.png';
+  static const String PASSWORD_HIDDEN_EYE_ICON_PATH =
+      'assets/icons/password_hidden_eye_icon.png';
+  static const String PASSWORD_SHOWN_EYE_ICON_PATH =
+      'assets/icons/password_shown_eye_icon.png';
+  static const String EXPLORE_ROUNDED_ICON_PATH =
+      'assets/icons/explore_rounded_icon.png';
+  static const String HOME_FEED_ROUNDED_ICON_PATH =
+      'assets/icons/home_feed_rounded_icon.png';
+  static const String NOTIFICATION_ROUNDED_ICON_PATH =
+      'assets/icons/notification_rounded_icon.png';
+  static const String REELS_ROUNED_ICON_PATH =
+      'assets/icons/reels_rounded_icon.png';
+  static const String USER_PROFILE_ROUNED_ICON_PATH =
+      'assets/icons/user_profile_rounded_icon.png';
+  static const String ACTIVE_EXPLORE_ROUNDED_ICON_PATH =
+      'assets/icons/active_explore_icon.png';
+  static const String ACTIVE_HOME_FEED_ROUNDED_ICON_PATH =
+      'assets/icons/active_home_feed_icon.png';
+  static const String ACTIVE_NOTIFICATION_ROUNDED_ICON_PATH =
+      'assets/icons/active_notification_icon.png';
+  static const String ACTIVR_REELS_ROUNED_ICON_PATH =
+      'assets/icons/active_reels_icon.png';
+  static const String ACTIVE_USER_PROFILE_ROUNED_ICON_PATH =
+      'assets/icons/active_user_profile_icon.png';
+  static const String ACCOUNT_LIST_DROPDOWN_ICON_PATH =
+      'assets/icons/account_list_dropdown_icon.png';
+  static const String ADD_MEDIUM_BUTTON_ICON_PATH =
+      'assets/icons/add_medium_button_icon.png';
+  static const String INSTAGRAM_ROUNDED_ICON_PATH =
+      'assets/icons/instagram_rounded_icon.png';
+  static const String PRIVATE_ACCOUNT_ICON_PATH =
+      'assets/icons/private_account_icon.png';
   static const String SIDE_MENU_ICON_PATH = 'assets/icons/side_menu_icon.png';
   static const String GRID_SELECT_ICON_PATH = 'assets/icons/grid_select.png';
-  static const String GRID_UNSELECT_ICON_PATH = 'assets/icons/grid_unselect.png';
-  static const String MENTION_SELECT_ICON_PATH = 'assets/icons/mention_select.png';
-  static const String MENTION_UNSELECT_ICON_PATH = 'assets/icons/mention_unselect.png';
+  static const String GRID_UNSELECT_ICON_PATH =
+      'assets/icons/grid_unselect.png';
+  static const String MENTION_SELECT_ICON_PATH =
+      'assets/icons/mention_select.png';
+  static const String MENTION_UNSELECT_ICON_PATH =
+      'assets/icons/mention_unselect.png';
   static const String FIREBASE_USER_STORAGE_PATH = '/user_storage';
   static const String FIREBASE_POST_STORAGE_PATH = '/post_storage';
   static const String FLASH_ICON_PATH = 'assets/icons/flash_icon.png';
   static const String NO_FLASH_ICON_PATH = 'assets/icons/no_flash_icon.png';
-  static const String SWAP_CAMERA_ICON_PATH = 'assets/icons/swap_camera_icon.png';
+  static const String SWAP_CAMERA_ICON_PATH =
+      'assets/icons/swap_camera_icon.png';
   static const String ARCHIVE_ICON_PATH = 'assets/icons/archive_icon.png';
   static const String ACTIVITY_ICON_PATH = 'assets/icons/activity_icon.png';
-  static const String CLOSE_FRIEND_LIST_ICON_PATH = 'assets/icons/close_friend_list_icon.png';
-  static const String COVID_CENTER_ICON_PATH = 'assets/icons/covid_center_icon.png';
-  static const String DISCOVER_PEOPLE_ICON_PATH = 'assets/icons/discover_people_icon.png';
+  static const String CLOSE_FRIEND_LIST_ICON_PATH =
+      'assets/icons/close_friend_list_icon.png';
+  static const String COVID_CENTER_ICON_PATH =
+      'assets/icons/covid_center_icon.png';
+  static const String DISCOVER_PEOPLE_ICON_PATH =
+      'assets/icons/discover_people_icon.png';
   static const String NAMETAG_ICON_PATH = 'assets/icons/nametag_icon.png';
-  static const String OPEN_FACEBOOK_ICON_PATH = 'assets/icons/open_facebook_icon.png';
+  static const String OPEN_FACEBOOK_ICON_PATH =
+      'assets/icons/open_facebook_icon.png';
   static const String SAVED_POST_ICON_PATH = 'assets/icons/saved_post_icon.png';
   static const String SETTING_ICON_PATH = 'assets/icons/setting_icon.png';
+  static const String CAMERA_ICON_PATH = 'assets/icons/camera_icon.png';
+  static const String ADD_FEED_BUTTON_ICON_PATH =
+      'assets/icons/add_feed_button_icon.png';
+  static const String MESSAGE_ICON_PATH = 'assets/icons/message_icon.png';
+  static const String LIKE_ICON_PATH = 'assets/icons/like_icon.png';
+  static const String LIKED_ICON_PATH = 'assets/icons/liked_icon.png';
+  static const String COMMENT_ICON_PATH = 'assets/icons/comment_icon.png';
+  static const String SAVE_ICON_PATH = 'assets/icons/save_icon.png';
+  static const String SAVED_ICON_PATH = 'assets/icons/saved_icon.png';
+  static const String SHARE_ICON_PATH = 'assets/icons/share_icon.png';
 }
