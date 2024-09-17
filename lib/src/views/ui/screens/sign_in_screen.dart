a74ba4621b44482d90401f406b29c4e50c116162
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-09-17 15:31:51 +0700

    style: Handle sign in facebok UI

--- lib/src/views/ui/screens/sign_in_screen.dart
+++ lib/src/views/ui/screens/sign_in_screen.dart
@@ -1,6 +1,11 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../business_logic/blocs/authentication/authentication_bloc.dart';
+import '../../../business_logic/blocs/authentication/authentication_event.dart';
+import '../../../business_logic/blocs/authentication/authentication_state.dart';
 import '../../utils/color_constant.dart';
 import '../../utils/dimension_constant.dart';
 import '../../utils/path_constant.dart';
@@ -20,74 +25,128 @@ class SignInScreen extends StatelessWidget {
       Navigator.of(context).pushNamed(RouteConstant.LOG_IN_SCREEN_ROUTE);
     }
 
-    void navigateToSignUpScreen(){
+    void navigateToSignUpScreen() {
       Navigator.of(context).pushNamed(RouteConstant.SIGN_UP_SCREEN_ROUTE);
     }
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset(
-                    PathConstant.INSTA_BLACK_LOGO_PATH,
-                    width: DimensionConstant.SIZE_175,
-                    height: DimensionConstant.SIZE_50,
-                  ),
-                  const SizedBox(
-                    height: DimensionConstant.SIZE_20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: DimensionConstant.SIZE_20,
-                    ),
-                    child: Text(
-                      StringConstant.SIGN_IN_INTRODUCE_LABEL,
-                      style: GoogleFonts.montserrat(
-                        color: ColorConstant.FF8E8E93,
-                        fontSize: DimensionConstant.SIZE_14,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Column(
+      body: SafeArea(
+        child: BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (BuildContext context, AuthenticationState state) {
+            if (state is AuthenticationInProgress) {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            if (state is AuthenticationSuccess) {
+              Navigator.of(context).pop();
+              final SnackBar snackBar = SnackBar(
+                elevation: 0,
+                behavior: SnackBarBehavior.floating,
+                backgroundColor: Colors.transparent,
+                content: AwesomeSnackbarContent(
+                  title: 'Success',
+                  message: 'Login successfully',
+                  contentType: ContentType.success,
+                ),
+              );
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(snackBar);
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                RouteConstant.HOME_SCREEN_ROUTE,
+                (Route<dynamic> route) => false,
+              );
+            }
+            if (state is AuthenticationFailed) {
+              Navigator.of(context).pop();
+              final SnackBar snackBar = SnackBar(
+                elevation: 0,
+                behavior: SnackBarBehavior.floating,
+                backgroundColor: Colors.transparent,
+                content: AwesomeSnackbarContent(
+                  title: 'Failed',
+                  message: 'Login with Facebook failed. Please try again.',
+                  contentType: ContentType.failure,
+                ),
+              );
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(snackBar);
+            }
+          },
+          child: Center(
+            child: Column(
               children: <Widget>[
-                SignInButtonWidget(
-                  onPressed: () {},
-                  iconPath: PathConstant.FACEBOOK_ICON_PATH,
-                  title: StringConstant.LOG_IN_WITH_FACEBOOK_TITLE,
-                  backgroundColor: ColorConstant.FF3797EF,
-                  textColor: ColorConstant.WHITE,
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        PathConstant.INSTA_BLACK_LOGO_PATH,
+                        width: DimensionConstant.SIZE_175,
+                        height: DimensionConstant.SIZE_50,
+                      ),
+                      const SizedBox(
+                        height: DimensionConstant.SIZE_20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: DimensionConstant.SIZE_20,
+                        ),
+                        child: Text(
+                          StringConstant.SIGN_IN_INTRODUCE_LABEL,
+                          style: GoogleFonts.montserrat(
+                            color: ColorConstant.FF8E8E93,
+                            fontSize: DimensionConstant.SIZE_14,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                const SizedBox(
-                  height: DimensionConstant.SIZE_26,
-                ),
-                const HorizontalOrLineWidget(),
-                const SizedBox(
-                  height: DimensionConstant.SIZE_26,
-                ),
-                OutlineSignInButtonWidget(
-                  title: StringConstant.SIGN_UP_WITH_PHONE_AND_EMAIL_TITLE,
-                  iconPath: '',
-                  onPressed: navigateToSignUpScreen,
-                ),
-                const SizedBox(
-                  height: DimensionConstant.SIZE_53,
-                ),
-                SignInFooterWidget(
-                  label: StringConstant.ALREADY_HAVE_AN_ACCOUNT_LABEL,
-                  actionLabel: StringConstant.SIGN_IN_LABEL,
-                  actionColor: ColorConstant.FF262626,
-                  onPressed: navigateToSocialLogInScreeen,
+                Column(
+                  children: <Widget>[
+                    SignInButtonWidget(
+                      onPressed: () {
+                        context
+                            .read<AuthenticationBloc>()
+                            .add(SignInWithFacebook());
+                      },
+                      iconPath: PathConstant.FACEBOOK_ICON_PATH,
+                      title: StringConstant.LOG_IN_WITH_FACEBOOK_TITLE,
+                      backgroundColor: ColorConstant.FF3797EF,
+                      textColor: ColorConstant.WHITE,
+                    ),
+                    const SizedBox(
+                      height: DimensionConstant.SIZE_26,
+                    ),
+                    const HorizontalOrLineWidget(),
+                    const SizedBox(
+                      height: DimensionConstant.SIZE_26,
+                    ),
+                    OutlineSignInButtonWidget(
+                      title: StringConstant.SIGN_UP_WITH_PHONE_AND_EMAIL_TITLE,
+                      iconPath: '',
+                      onPressed: navigateToSignUpScreen,
+                    ),
+                    const SizedBox(
+                      height: DimensionConstant.SIZE_53,
+                    ),
+                    SignInFooterWidget(
+                      label: StringConstant.ALREADY_HAVE_AN_ACCOUNT_LABEL,
+                      actionLabel: StringConstant.SIGN_IN_LABEL,
+                      actionColor: ColorConstant.FF262626,
+                      onPressed: navigateToSocialLogInScreeen,
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
