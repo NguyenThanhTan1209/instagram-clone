eae794e8a58671da3b1b25108dd82694ff3b7139
Tan Nguyen <nguyenthanhtan1298@gmail.com>
2024-09-24 18:36:26 +0700

    fix: Fix how to media show when user just post 1 media

--- lib/src/views/ui/widgets/post_media_widget.dart
+++ lib/src/views/ui/widgets/post_media_widget.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import '../../../business_logic/models/post.dart';
 import '../../utils/color_constant.dart';
 import '../../utils/dimension_constant.dart';
+import 'media_item_widget.dart';
 
-class PostMediaWidget extends StatelessWidget {
+class PostMediaWidget extends StatefulWidget {
   const PostMediaWidget({
     super.key,
     required this.post,
@@ -18,73 +19,77 @@ class PostMediaWidget extends StatelessWidget {
   final ValueNotifier<int> postIndex;
 
   @override
+  State<PostMediaWidget> createState() => _PostMediaWidgetState();
+}
+
+class _PostMediaWidgetState extends State<PostMediaWidget> {
+  @override
   Widget build(BuildContext context) {
     final double screenWith = MediaQuery.sizeOf(context).width;
 
     return Stack(
       children: <Widget>[
-        CarouselSlider.builder(
-          itemCount: post.images.length,
-          itemBuilder: (
-            BuildContext context,
-            int index,
-            int realIndex,
-          ) {
-            final String imageUrl = post.images[index];
-            return Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
-              width: screenWith,
-              height: screenWith,
-            );
-          },
-          options: CarouselOptions(
-            height: screenWith,
-            viewportFraction: 1,
-            onPageChanged: (
+        if (widget.post.images.length == 1)
+          MediaItemWidget(mediaUrl: widget.post.images.first)
+        else
+          CarouselSlider.builder(
+            itemCount: widget.post.images.length,
+            itemBuilder: (
+              BuildContext context,
               int index,
-              CarouselPageChangedReason reason,
+              int realIndex,
             ) {
-              postIndex.value = index + 1;
+              final String imageUrl = widget.post.images[index];
+              return MediaItemWidget(mediaUrl: imageUrl);
             },
-          ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            margin: const EdgeInsets.all(
-              DimensionConstant.SIZE_14,
-            ),
-            padding: const EdgeInsets.symmetric(
-              vertical: DimensionConstant.SIZE_6,
-              horizontal: DimensionConstant.SIZE_8,
-            ),
-            decoration: BoxDecoration(
-              color: ColorConstant.FF121212.withOpacity(
-                DimensionConstant.SIZE_0_POINT_7,
-              ),
-              borderRadius: BorderRadius.circular(
-                DimensionConstant.SIZE_13,
-              ),
-            ),
-            child: ValueListenableBuilder<int>(
-              valueListenable: postIndex,
-              builder: (
-                BuildContext context,
-                int value,
-                Widget? child,
+            options: CarouselOptions(
+              height: screenWith,
+              viewportFraction: 1,
+              onPageChanged: (
+                int index,
+                CarouselPageChangedReason reason,
               ) {
-                return Text(
-                  '$value/${post.images.length}',
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        fontSize: DimensionConstant.SIZE_12,
-                        color: ColorConstant.WHITE,
-                      ),
-                );
+                widget.postIndex.value = index + 1;
               },
             ),
           ),
-        ),
+        if (widget.post.images.length>1)
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: const EdgeInsets.all(
+                DimensionConstant.SIZE_14,
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: DimensionConstant.SIZE_6,
+                horizontal: DimensionConstant.SIZE_8,
+              ),
+              decoration: BoxDecoration(
+                color: ColorConstant.FF121212.withOpacity(
+                  DimensionConstant.SIZE_0_POINT_7,
+                ),
+                borderRadius: BorderRadius.circular(
+                  DimensionConstant.SIZE_13,
+                ),
+              ),
+              child: ValueListenableBuilder<int>(
+                valueListenable: widget.postIndex,
+                builder: (
+                  BuildContext context,
+                  int value,
+                  Widget? child,
+                ) {
+                  return Text(
+                    '$value/${widget.post.images.length}',
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          fontSize: DimensionConstant.SIZE_12,
+                          color: ColorConstant.WHITE,
+                        ),
+                  );
+                },
+              ),
+            ),
+          ),
       ],
     );
   }
